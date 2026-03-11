79c255aa7daeb131e4ac9188e94cca72bfcc284d
ImpactAcademie <impactacademiestudent@gmail.com>
2026-03-11 19:11:33 +0100

    l10

---
+++ phileas wilkin/script.js
@@ -0,0 +1,9 @@
+moneybtn = document.getElementById("money-btn");
+
+
+
+moneybtn.addEventListener("click",() => {
+    texte = document.getElementById("result")
+
+    texte.innerHTML = "Merci"
+});
