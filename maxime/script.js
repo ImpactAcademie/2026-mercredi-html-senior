895e7e2725aac4a27407335b7e073d7a9754d52c
student <student@gmail.com>
2026-03-11 19:15:06 +0100

    l10

---
+++ maxime/script.js
@@ -0,0 +1,11 @@
+console.log("hello !")
+
+moneyBtn = document.getElementyById("money-btn")
+
+
+moneyBtn.addEventListener("click", () => {
+    textElement = document.getElementById(don pharmacie)
+
+    reponseDon = document.getElementById("reponse-don")
+    reponseDon.innerHTML = "vous avez donné: " +  texteElement.value
+})
